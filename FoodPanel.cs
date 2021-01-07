b0e736e80ebba60b835f29bba35793b75a511659
Mihoc Andreea <48786285+MihocAndreea@users.noreply.github.com>
2021-01-07 08:47:22 -0800

    Add files via upload

---
+++ FoodPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodPanel : MonoBehaviour
+{
+    public GameObject FPanel;
+
+    public void Food()
+    {
+        if (FPanel != null)
+        {
+            bool isActive = FPanel.activeSelf;
+            FPanel.SetActive(!isActive);
+        }
+    }
+}
